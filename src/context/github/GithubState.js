e04d6057b21c0354ac9c03f042c9a7edc6653cf0
Stanislav <gromila12@rambler.ru>
2022-09-12 15:30:15 +0600

    get data from github

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -13,6 +13,11 @@ import {
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
+const withCreds = (url) => {
+  //return `${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+  return url;
+};
+
 export const GithubState = ({ children }) => {
   const initialState = {
     user: {},
@@ -25,10 +30,11 @@ export const GithubState = ({ children }) => {
 
   const search = async (value) => {
     setLoading();
+
     const response = await axios.get(
-      `https://api.github.com/search/users?q=${value}`
-      //   `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+      withCreds(`https://api.github.com/search/users?q=${value}`) //&
     );
+
     dispatch({
       type: SEARCH_USERS,
       payload: response.data.items,
@@ -38,22 +44,26 @@ export const GithubState = ({ children }) => {
   const getUser = async (name) => {
     setLoading();
 
-    // ...
+    const response = await axios.get(
+      withCreds(`https://api.github.com/users/${name}`) //?
+    );
 
     dispatch({
       type: GET_USER,
-      payload: {},
+      payload: response.data,
     });
   };
 
   const getRepos = async (name) => {
     setLoading();
 
-    // ...
+    const response = await axios.get(
+      withCreds(`https://api.github.com/users/${name}/repos?per_page=5`) //&
+    );
 
     dispatch({
       type: GET_REPOS,
-      payload: [],
+      payload: response.data,
     });
   };
 
