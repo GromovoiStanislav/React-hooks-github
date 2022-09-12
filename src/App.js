46427c19673464043760a5ef4e11b7cba1bc0056
Stanislav <gromila12@rambler.ru>
2022-09-12 11:40:23 +0600

    pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Profile } from './pages/Profile';
+
 const App = (props) => {
   return (
-    <div className="container pt-4">
-      <h1>Hello!</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container pt-4">
+        <Home />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+export const Navbar = () => (
+  <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
+    <div className="navbar-brand">GitHub поиск</div>
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <span className="nav-link">Главная</span>
+      </li>
+      <li className="nav-item">
+        <span className="nav-link">Информация</span>
+      </li>
+    </ul>
+  </nav>
+);
