210f627c2fe6663f2294a3802c849d59d28abf53
Stanislav <gromila12@rambler.ru>
2022-09-12 13:19:50 +0600

    component alert

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,21 @@
+import { useContext } from 'react';
+import { AlertContext } from '../context/alert/alertContext';
+
 export const Search = () => {
+  const { show } = useContext(AlertContext);
+  const onSubmit = (event) => {
+    if (event.keyCode === 13) {
+      show(event.target.value);
+    }
+  };
+
   return (
     <div className="form-group mb-4">
       <input
         type="text"
         className="form-control"
         placeholder="Введите ник пользователя..."
+        onKeyDown={onSubmit}
       />
     </div>
   );
